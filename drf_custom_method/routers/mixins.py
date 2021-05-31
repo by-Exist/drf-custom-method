5f8a0d2fa5861366a9962fd9ce762a93f490d8a5
Jongbeom-Kwon <bolk9652@naver.com>
2021-05-31 21:01:37 +0900

    Fix lookup_value_regex behave

--- drf_custom_method/routers/mixins.py
+++ drf_custom_method/routers/mixins.py
@@ -6,12 +6,19 @@ from rest_framework_nested.routers import NestedMixin
 
 
 class CustomMethodMixin:
+    def __init__(self) -> None:
+        self.slash_set = set()
+        super().__init__()
+
     def _get_dynamic_route(self, route, action):
         initkwargs = route.initkwargs.copy()
         initkwargs.update(action.kwargs)
 
         url_path = escape_curly_brackets(action.url_path)
         slash = action.slash  # here
+
+        # here
+        self.slash_set.add(slash)
 
         return Route(
             url=route.url.replace("{url_path}", url_path).replace(
@@ -22,3 +29,17 @@ class CustomMethodMixin:
             detail=route.detail,
             initkwargs=initkwargs,
         )
+
+    def get_lookup_regex(self, viewset, lookup_prefix=""):
+        base_regex = "(?P<{lookup_prefix}{lookup_url_kwarg}>{lookup_value})"
+        lookup_field = getattr(viewset, "lookup_field", "pk")
+        lookup_url_kwarg = getattr(viewset, "lookup_url_kwarg", None) or lookup_field
+        # here
+        lookup_value_regex = "[^/.{}]+".format("".join(self.slash_set))
+        lookup_value = getattr(viewset, "lookup_value_regex", lookup_value_regex)
+        result = base_regex.format(
+            lookup_prefix=lookup_prefix,
+            lookup_url_kwarg=lookup_url_kwarg,
+            lookup_value=lookup_value,
+        )
+        return result
